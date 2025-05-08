bfe580e4b84a9c34a8908e4acd96d7df19880fc8
gabrielalmir <gabr.almir@gmail.com>
2025-05-08 19:39:42 -0300

    feat: add jsdocs to d4sign error class

--- src/errors/d4sign.error.ts
+++ src/errors/d4sign.error.ts
@@ -1,7 +1,26 @@
+/**
+ * Represents a custom error specific to D4Sign operations.
+ * Extends the built-in `Error` class to include additional properties
+ * such as HTTP status code and response data.
+ */
 export class D4SignError extends Error {
+    /**
+     * The HTTP status code associated with the error.
+     */
     status: number;
+
+    /**
+     * Additional data or payload related to the error.
+     */
     data: any;
 
+    /**
+     * Creates an instance of `D4SignError`.
+     *
+     * @param message - A descriptive error message.
+     * @param status - The HTTP status code associated with the error.
+     * @param data - Additional data or payload related to the error.
+     */
     constructor(message: string, status: number, data: any) {
         super(message);
         this.name = 'D4SignError';
